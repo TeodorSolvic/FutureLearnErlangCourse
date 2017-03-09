7f431fe78d59f9672ea779f1c9b18512508f87a4
TeodorSolvic <trip.of.neo@gmail.com>
2017-03-09 20:49:59 +0200

    work on assignment

--- Practice/2.20/worker.erl
+++ Practice/2.20/worker.erl
@@ -7,21 +7,61 @@
 %% API
 -export([process/1]).
 
-
-%% test(N)->
-%%   S = index:get_file_contents(N),
-%%   index:show_file_contents(S).
-
 process(F)->
   S = index:get_file_contents(F),
-  lower_case(lists:append(get_words(S))).
+  WORDS_LISTS = get_words(S),
+  CLEAR_WORDS_LISTS = clear_words(WORDS_LISTS),
+  R = lists:usort(lists:append(CLEAR_WORDS_LISTS)),
+  entry(R, CLEAR_WORDS_LISTS).
+
+entry([], _S)->[];
+entry([X|Xs], S)->[{X, lists:reverse(occurs(X, S))}|entry(Xs, S)].
 
 
+%% sort_occurs([X|Xs])->sort_occurs([X|Xs],X,[]).
+%% sort_occurs([],_,Acc)->Acc;
+%% sort_occurs([X],_,Acc)->[{X,X}|Acc];
+%% sort_occurs([X,Y|Xs], _S, Acc) when Y == X + 1 ->
+%%   sort_occurs(Xs, X, Acc);
+%% sort_occurs([X,Y|Xs], S, Acc)->
+%%   sort_occurs([Y|Xs], Y, [{S,X}|Acc]).
 
-lower_case([])->[];
-lower_case([X|Xs])->
-  [string:to_lower(X)|lower_case(Xs)].
+
+occurs(X, S)->occurs(X, S, [], 1).
+occurs(_X, [], Acc, _I)->Acc;
+occurs(X, [Y|Ys], Acc, I) ->
+  case lists:member(X, Y) of
+    true -> occurs(X, Ys, [I|Acc], I+1);
+    false -> occurs(X, Ys, Acc, I+1)
+  end.
+
+
+clear_words([])->[];
+clear_words([X|Xs])->[remove_short(lower_case(remove_non_chars(X)))|clear_words(Xs)].
 
 get_words([])->[];
-get_words([X|Xs])->
-  [string:tokens(X, " ")|get_words(Xs)].
+get_words([X|Xs]) -> [string:tokens(X, " ")|get_words(Xs)].
+
+lower_case([]) -> [];
+lower_case([X|Xs]) -> [string:to_lower(X)|lower_case(Xs)].
+
+remove_short([])->[];
+remove_short([X|Xs])->
+  case X of
+    [_A,_B,_C|[]]->remove_short(Xs);
+    [_A,_B|[]]->remove_short(Xs);
+    [_A|[]]->remove_short(Xs);
+    _->[X|remove_short(Xs)]
+  end.
+
+remove_non_chars([])->[];
+remove_non_chars([X|Xs])->
+  C = clear_str(X),
+  case C of
+    [] -> remove_non_chars(Xs);
+    _ -> [C|remove_non_chars(Xs)]
+  end.
+
+clear_str([])->[];
+clear_str([X|Xs]) when X >= 65, X =< 90 ; X >= 97, X =< 122 -> [X|clear_str(Xs)];
+clear_str([_X|Xs]) -> clear_str(Xs).
